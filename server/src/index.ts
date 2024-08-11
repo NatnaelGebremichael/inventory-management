a9c601d80457b7507744993ea71e8cf0f9c5ae17
Natnael Gebremichael <natnael92460@gmail.com>
2024-08-11 18:01:56 +1200

    cider block, pm2 config

--- server/src/index.ts
+++ server/src/index.ts
@@ -32,8 +32,8 @@ app.use("/users", userRoutes);
 app.use("/expenses", expenseRoutes);
 
 // Server
-const port = process.env.PORT || 3001;
-app.listen(port, () => {
+const port = Number(process.env.PORT) || 3001;
+app.listen(port, "0.0.0.0",  () => {
     console.log(`server running on port ${port}`)
 });
 
